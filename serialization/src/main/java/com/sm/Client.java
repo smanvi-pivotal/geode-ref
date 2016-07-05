0695918e4e73e37706b57b4f670785a7a74e7a51
Srikanth Manvi <srikanthmanvi@gmail.com>
2016-07-05 11:26:26 -0400

    SDG

--- serialization/src/main/java/com/sm/Client.java
+++ serialization/src/main/java/com/sm/Client.java
@@ -1,7 +1,18 @@
 package com.sm;
 
+import com.gemstone.gemfire.cache.Region;
+import com.sm.geode.ref.domain.Customer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by smanvi on 6/30/16.
  */
 public class Client {
+
+    public static void main(String args[]) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("client/cache-config.xml");
+        Region<Object, Customer> customerRegion = (Region<Object, Customer>) ctx.getBean("Customers");
+        Customer c = customerRegion.get(1);
+        System.out.println(" RETRIVED :"+c);
+    }
 }
--- serialization/src/main/java/com/sm/Server.java
+++ serialization/src/main/java/com/sm/Server.java
@@ -14,8 +14,8 @@ public class Server {
 
     public static void main(String args[]) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("server/cache-config.xml");
-        Cache cache = context.getBean(Cache.class);
-        Region<Object, Object> customerRegion = cache.getRegion("Customers");
+        Region<Object, Customer> customerRegion = (Region<Object, Customer>) context.getBean("Customers");
+//        Region<Object, Customer> customerRegion = cache.getRegion("Customers");
         customerRegion.put(1,new Customer("John","Doe","NewYork"));
         System.out.println();
         System.out.println("Hit Enter to exit");
